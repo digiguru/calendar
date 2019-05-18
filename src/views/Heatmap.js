dd0bbe2a4d3b63e45be3a64641d98f989407d9be
Adam Hall <ahall@prmaconsulting.com>
2019-05-18 22:42:57 +0100

    Adds a kind of better grid styling

--- src/views/Heatmap.js
+++ src/views/Heatmap.js
@@ -2,46 +2,108 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+//import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
   },
   paper: {
+    margin: theme.spacing(1),
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
 });
+const styleContainer = (columns, rows) => {
+  const colText = Array(columns).fill("[column] 1fr").join(" ");
+  const rowText = Array(rows).fill("[row] 1fr").join(" ");
+  return {
+    display: 'grid',
+    border: '1px',
+    gridTemplateColumns: `[first] 400px ${colText}`,
+    gridTemplateRows: `[first] 60px ${rowText}`,
+    gridAutoFlow: "column" 
+  }
+//    gridTemplateColumns: `[first] 40px repeat(${columns}, 1fr [column])`
 
+};
+const styleCell = (column, row) => {
+  return {
+    gridColumn: column,
+    gridRow: row,    
+  }
+}
+const styleGreen = {
+    border: "1px green solid"
+  };
+const styleYellow = {
+    border: "1px yellow solid"
+  }
+const styleRed = {
+    border: "1px red solid"
+  };
+  const getRatingStyle = (rating) => {
+    return rating > 66 ? styleGreen : (rating < 34 ? styleRed : styleYellow);
+  }
+  
 function Heatmap(props) {
   const { classes } = props;
   
-  const areas = [
-    "Area 1",
-    "Area 2"
+  const rawData = [
+    {area:"Spain", data: [
+      {domain:"Cost", value: "£", rating:100},
+      {domain:"Distance", value: 1000, rating: 60},
+      {domain:"Language", value: "Spanish", rating: 50}
+    ]},
+    {area:"Thailand", data: [
+      {domain:"Cost", value: "£££", rating:10},
+      {domain:"Distance", value: 5855, rating: 10},
+      {domain:"Language", value: "Thai", rating: 10}
+    ]},
+    {area:"Wales", data: [
+      {domain:"Cost", value: "£", rating:100},
+      {domain:"Distance", value: 187, rating: 80},
+      {domain:"Language", value: "Spanish", rating: 50}
+    ]}
   ];
 
-  const rawData = [
-    {domain:"Domain 1", data:["Good", "Bad"] },
-    {domain:"Domain 2", data:["Bad", "Bad"] },
-    {domain:"Domain 3", data:["Good", "Good"] }
-  ]
+  const domains = rawData[0].data.map(x => x.domain);
+  
+  const areas = rawData.map(x => x.area);
+  const cells = [];
+  areas.forEach((area, areaIndex) => {
+    const g = rawData[areaIndex].data;
+    const h = g.map(x => ({rating: x.rating, value: x.value}));
+    cells.push(h);
+  });
+  console.log(cells);
   return (
-    <div className={classes.root}>
-       
-      <Grid container spacing={3}>
-        <Grid item>
-          <Paper className={classes.paper}>Domains</Paper>
-        </Grid>
-      {areas.map((item, index) => (
-        <Grid item>
-          <Paper className={classes.paper}>{item}</Paper>
-        </Grid>
+    <div style={styleContainer(areas.length, domains.length)}>
+    
+        <Paper style={styleCell(1,1)} className={classes.paper}>Domains</Paper>
+    
+    
+      {domains.map((item, index) => (
+        
+          <Paper key={"domain" + index} style={styleCell(1,index+2)} className={classes.paper}>{item}</Paper>
+        
       ))}
-      </Grid>
-        {rawData.map(({domain, data}, index) => (
+      {areas.map((area, areaIndex) => (
+          <React.Fragment key={"area" + areaIndex}>
+              <Paper style={styleCell(areaIndex+2,1)}  className={classes.paper}>{area}</Paper>
+            {cells[areaIndex].map(({value, rating}, indexCell) => (
+              <Paper key={"cell" + areaIndex + "-" + indexCell} style={ {...styleCell(areaIndex+2,indexCell+2), ...getRatingStyle(rating) }} className={classes.paper}>{value}</Paper>
+              
+            ))}
+            </React.Fragment>
+      ))}
+        
+  
+    </div>
+  );
+  /*
+  {rawData.map(({domain, data}, index) => (
           <Grid container  spacing={3}>>
             <Grid item>
               <Paper className={classes.paper}>{domain}</Paper>
@@ -53,10 +115,7 @@ function Heatmap(props) {
             ))}
             </Grid>
           ))}
-        
-   
-    </div>
-  );
+  */
 }
 
 Heatmap.propTypes = {
