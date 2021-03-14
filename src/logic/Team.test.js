bfa0bb93264a577e71edbcebea57e49e99b21476
digiguru <digiguru@gmail.com>
2021-03-14 22:25:46 +0000

    Fixes test names

--- src/logic/Team.test.js
+++ src/logic/Team.test.js
@@ -1,18 +1,18 @@
 import { Team } from "./Team";
 
 describe('The Team', () => {
-    it('Can be created with a name', () => {
+    it('Can be created with a name like "Nav"', () => {
         const input = new Team("Nav");
         
         expect(input.name).toBe("Nav");
     });
-    it('Can can have capabilties', () => {
+    it('Can have capabilties like "FE"', () => {
         const input = new Team("Nav", ["FE"]);
     
         expect(input.capabilities.length).toBe(1);
     });
 
-    it('Can cannot have duplicate capabilities', () => {
+    it('Cannot have duplicate capabilities, it just returns 1 of them', () => {
         const input = new Team("Nav", ["FE", "FE"]);
     
         expect(input.capabilities.length).toBe(1);
