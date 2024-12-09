5e7aaeb3eb15fac8393de8eaf872edf7e5332f7d
digiguru <digiguru@gmail.com>
2024-12-09 15:13:21 +0000

    Updates testing library code

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { AppHolder } from './AppHolder';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AppHolder />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const root = createRoot(div);
+  root.render(<AppHolder />);
+  root.unmount();
 });
